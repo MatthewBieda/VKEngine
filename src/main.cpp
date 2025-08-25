6690585037a013158d8f026494fa4b34be4a7404
Matthew Bieda <matthew.bieda@gmail.com>
2025-08-25 21:51:40 +0100

    Made the device initialization more robust

--- src/main.cpp
+++ src/main.cpp
@@ -89,11 +89,12 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 	GLFWwindow* window = glfwCreateWindow(1920, 1080, "VulkanEngine", nullptr, nullptr);
 
-	VkSurfaceKHR surface = nullptr;;
+	VkSurfaceKHR surface = nullptr;
 	VkResult err = glfwCreateWindowSurface(instance, window, nullptr, &surface);
 	if (err)
 	{
 		std::cout << "Window creation failed" << err << std::endl;
+		return -1;
 	}
 
 	// Get physical devices
@@ -103,20 +104,127 @@ int main()
 	std::vector<VkPhysicalDevice> devices(numDevices);
 	vkEnumeratePhysicalDevices(instance, &numDevices, devices.data());
 
-	// Just use the first device
-	VkPhysicalDevice physicalDevice = devices[0];
+	// Score each device
+	VkPhysicalDevice bestDevice = VK_NULL_HANDLE;
+	int bestScore = -1;
+	for (const VkPhysicalDevice& device: devices)
+	{
+		VkPhysicalDeviceProperties properties;
+		vkGetPhysicalDeviceProperties(device, &properties);
 
-	// Specify queue creation info (assume queue family 0 exists and supports graphics)
+		int score = 0;
+
+		// Prefer discrete GPUs
+		if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+		{
+			score += 1000;
+		}
+
+		if (score > bestScore)
+		{
+			bestScore = score;
+			bestDevice = device;
+		}
+	}
+
+	// Enumerate device extensions and ensure swapchain support
+	uint32_t extensionCount = 0;
+	vkEnumerateDeviceExtensionProperties(bestDevice, nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+	vkEnumerateDeviceExtensionProperties(bestDevice, nullptr, &extensionCount, availableExtensions.data());
+
+	bool swapchainSupported = false;
+	for (const VkExtensionProperties& extension : availableExtensions)
+	{
+		if (strcmp(extension.extensionName, VK_KHR_SWAPCHAIN_EXTENSION_NAME) == 0)
+		{
+			swapchainSupported = true;
+			break;
+		}
+	}
+
+	if (!swapchainSupported)
+	{
+		std::cerr << "Selected device does not support VK_KHR_swapchain!" << std::endl;
+		return -1;
+	}
+
+	// Configure device extensions
+	std::vector<const char*> deviceExtensions = { "VK_KHR_swapchain" };
+
+	// Enable specific GPU features if we need them
+	VkPhysicalDeviceFeatures deviceFeatures{};
+	//vkGetPhysicalDeviceFeatures(physDev, &deviceFeatures);
+
+	// Query queue families
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(bestDevice, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(bestDevice, &queueFamilyCount, queueFamilies.data());
+
+	// Find a queue that supports graphics and present
+	int graphicsQueueFamilyIndex = -1;
+	int presentQueueFamilyIndex = -1;
+
+	for (int i = 0; i < queueFamilies.size(); ++i)
+	{
+		if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			graphicsQueueFamilyIndex = i;
+		}
+
+		// Query presentation support
+		VkBool32 presentSupport = VK_FALSE;
+		vkGetPhysicalDeviceSurfaceSupportKHR(bestDevice, i, surface, &presentSupport);
+		if (presentSupport)
+		{
+			presentQueueFamilyIndex = i;
+		}
+
+		// Early exit if both capabilities found in the same family
+		if (graphicsQueueFamilyIndex != -1 && presentQueueFamilyIndex != -1)
+		{
+			break;
+		}
+	}
+
+	if (graphicsQueueFamilyIndex == -1)
+	{
+		std::cerr << "No queue family supports graphics!" << std::endl;
+		return -1;
+	}
+
+	if (presentQueueFamilyIndex == -1)
+	{
+		std::cerr << "Presentation not supported!" << std::endl;
+		return -1;
+	}
+
+	if (graphicsQueueFamilyIndex != presentQueueFamilyIndex)
+	{
+		std::cerr << "Graphics queue cannot support presentation" << std::endl;
+		return -1;
+	}
+
+	const VkQueueFamilyProperties qf = queueFamilies[graphicsQueueFamilyIndex];
+
+	std::cout << "Queue family index: " << graphicsQueueFamilyIndex << std::endl;
+	std::cout << "Queue count: " << qf.queueCount << std::endl;
+	std::cout << "Queue flags: " << qf.queueFlags << std::endl;
+
+	if (qf.queueFlags & VK_QUEUE_GRAPHICS_BIT)  std::cout << "    - Graphics" << std::endl;
+	if (qf.queueFlags & VK_QUEUE_COMPUTE_BIT)   std::cout << "    - Compute" << std::endl;
+	if (qf.queueFlags & VK_QUEUE_TRANSFER_BIT)  std::cout << "    - Transfer" << std::endl;
+	if (qf.queueFlags & VK_QUEUE_SPARSE_BINDING_BIT) std::cout << "    - Sparse binding" << std::endl;
+
+	// Specify queue creation info using the right family
 	float queuePriority = 1.0f;
 	VkDeviceQueueCreateInfo queueCreateInfo{};
 	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-	queueCreateInfo.queueFamilyIndex = 0;
+	queueCreateInfo.queueFamilyIndex = graphicsQueueFamilyIndex;
 	queueCreateInfo.queueCount = 1;
 	queueCreateInfo.pQueuePriorities = &queuePriority;
-
-	// Specify device features we want
-	VkPhysicalDeviceFeatures deviceFeatures{};
-	//vkGetPhysicalDeviceFeatures(physDev, &deviceFeatures);
 
 	// Specify logical device create info
 	VkDeviceCreateInfo deviceCreateInfo{};
@@ -124,15 +232,12 @@ int main()
 	deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
 	deviceCreateInfo.queueCreateInfoCount = 1;
 	deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
-
-	// Extensions (required for swapchain)	
-	std::vector<const char*> deviceExtensions = { "VK_KHR_swapchain" };
 	deviceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
 	deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
 	// Create a logical device
 	VkDevice device = VK_NULL_HANDLE;
-	if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS)
+	if (vkCreateDevice(bestDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS)
 	{
 		std::cerr << "Failed to create logical device!" << std::endl;
 		return -1;
@@ -140,12 +245,14 @@ int main()
 
 	// Get the graphics queue (assume queue 0 from family 0)
 	VkQueue graphicsQueue = VK_NULL_HANDLE;
-	vkGetDeviceQueue(device, 0, 0, &graphicsQueue);
+	vkGetDeviceQueue(device, graphicsQueueFamilyIndex, 0, &graphicsQueue);
 
 	// Also load device functions from the driver directly for around 7% speedup
 	volkLoadDevice(device);
 
 	std::cout << "Logical device and graphics queue created successfully" << std::endl;
+
+
 
 	while (!glfwWindowShouldClose(window))
 	{
